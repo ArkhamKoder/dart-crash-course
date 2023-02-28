f524f9b0a3507b7259d8984a93ab670b095835c0
Arkham Koder <johnbasilogbhemhe@gmail.com>
2023-02-28 13:40:54 +0100

    binary infix

---
+++ example4.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  // Binary infix
+  const age = 50;
+  print(age + 20);
+  print(age - 20);
+  print(age * 20);
+  print(age / 20);
+  print(age ~/ 20);
+  print(age == 20);
+  print(age != 20);
+  print(age > 20);
+  print(age < 20);
+  print(age >= 20);
+  print(age <= 20);
+}
