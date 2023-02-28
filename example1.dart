2961754949da2a3d9557252797e20760f75a578c
Arkham Koder <johnbasilogbhemhe@gmail.com>
2023-02-28 12:32:24 +0100

    codes

---
+++ example1.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  const age1 = 10;
+  const age2 = 5;
+  print(age1 + age2);
+  print(age1 - age2);
+  print(age1 * age2);
+  print(age1 / age2);
+  print(age1 ~/ age2); //truncating divison
+}
