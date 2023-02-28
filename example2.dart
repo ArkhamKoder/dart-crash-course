28bd77bd47fe884016a90270921ef70a6c6a2571
Arkham Koder <johnbasilogbhemhe@gmail.com>
2023-02-28 12:52:57 +0100

    unary prefix

---
+++ example2.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  // Unary prefix , unary postfix, binary infix , compund assignment
+  int age = 30;
+  print(--age);
+  print(!true);
+  print(++age);
+  print(-age);
+}
