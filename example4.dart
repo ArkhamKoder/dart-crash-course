f3da736fb2ce52ed91f716a0a60b74ba0fb20da6
Arkham Koder <johnbasilogbhemhe@gmail.com>
2023-03-01 11:51:25 +0100

    bitwise infix operators

--- example4.dart
+++ example4.dart
@@ -12,4 +12,13 @@ void main(List<String> args) {
   print(age < 20);
   print(age >= 20);
   print(age <= 20);
+  print(age);
+  // bitwise infix operators
+
+  print(age & 20); // bitwise AND
+  print(age | 20); // bitwise OR
+  print(age ^ 20); // bitwise XOR
+
+  print(age << 20); //bitwise left
+  print(age >> 20); //bitwise right
 }
